281e1bcb93b19921881422c32be75d4f88360de6
gerald-gv <geraldgutierrez918@gmail.com>
2026-01-29 12:04:05 -0500

    Inicializando Proyecto Web-App Domotics

---
+++ src/main/java/com/domoticsweb/proy_appweb_LPII/ProyAppwebLpiiApplication.java
@@ -0,0 +1,13 @@
+package com.domoticsweb.proy_appweb_LPII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyAppwebLpiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyAppwebLpiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domoticsweb/proy_appweb_LPII/ProyAppwebLpiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domoticsweb.proy_appweb_LPII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyAppwebLpiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
